9f644e11fa6e237e4db077ca11ffbb1a59fe8a25
Rezrazi <Rezrazi@users.noreply.github.com>
2023-05-24 18:09:24 +0000

    Fix styling

--- tests/Events/PushEventTest.php
+++ tests/Events/PushEventTest.php
@@ -6,7 +6,7 @@ use Oneduo\LaravelGitlabWebhookClient\Events\PushEvent;
 it('dispatches an event for IssueEvent', function () {
     Event::fake();
 
-    sendWebhook(__DIR__ . '/../fixtures/push.json')
+    sendWebhook(__DIR__.'/../fixtures/push.json')
         ->assertNoContent();
 
     Event::assertDispatched(PushEvent::class);
