139976fa00af4e677322e2517fb91e478bd0cce4
Greg Korba <greg@codito.dev>
2023-12-06 17:02:24 +0100

    fix: Jobs' duration and runner may be null (#10)
    * Job duration may be null
    
    * Job runner may be null
    
    * Support for `merge_commit_sha` in Merge Request event

--- src/Data/Job.php
+++ src/Data/Job.php
@@ -21,14 +21,14 @@ class Job extends Data
         public readonly ?Carbon $started_at,
         #[WithCast(GitlabUTCDatetimeCast::class)]
         public readonly ?Carbon $finished_at,
-        public readonly float $duration,
-        public readonly float $queued_duration,
+        public readonly ?float $duration,
+        public readonly ?float $queued_duration,
         public readonly ?string $failure_reason,
         public readonly string $when,
         public readonly bool $manual,
         public readonly bool $allow_failure,
         public readonly User $user,
-        public readonly Runner $runner,
+        public readonly ?Runner $runner,
         public readonly ArtifactsFile $artifacts_file,
         public readonly ?Environment $environment,
     ) {
