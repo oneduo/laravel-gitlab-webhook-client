e63f0871267894bd90a02d8c0d32e0ad09be6f3d
Greg Korba <greg@codito.dev>
2023-12-06 17:40:28 +0100

    Support for `detailed_merge_status` in Merge Request event

---
+++ src/Enums/DetailedMergeStatus.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oneduo\LaravelGitlabWebhookClient\Enums;
+
+/**
+ * @see https://docs.gitlab.com/ee/api/merge_requests.html#merge-status
+ */
+enum DetailedMergeStatus: string
+{
+    case BLOCKED_STATUS = 'blocked_status';
+    case BROKEN_STATUS = 'broken_status';
+    case CHECKING = 'checking';
+    case CI_MUST_PASS = 'ci_must_pass';
+    case CI_STILL_RUNNING = 'ci_still_running';
+    case DISCUSSIONS_NOT_RESOLVED = 'discussions_not_resolved';
+    case DRAFT_STATUS = 'draft_status';
+    case EXTERNAL_STATUS_CHECKS = 'external_status_checks';
+    case JIRA_ASSOCIATION_MISSING = 'jira_association_missing';
+    case MERGEABLE = 'mergeable';
+    case NOT_APPROVED = 'not_approved';
+    case NOT_OPEN = 'not_open';
+    case POLICIES_DENIED = 'policies_denied';
+    case UNCHECKED = 'unchecked';
+}
