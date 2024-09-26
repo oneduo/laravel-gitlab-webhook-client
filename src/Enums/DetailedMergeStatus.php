78e802a63bdd4dfad4a84dcaf1464101a6a9b1e6
Greg Korba <greg@codito.dev>
2024-09-26 16:17:49 +0200

    Add missing enum values for detailed Merge Request status (#20)
    * Add missing enum values for detailed Merge Request status
    
    * Add `requested_changes` as possible detailed merge status value
    
    INFO: This value does not exist in https://gitlab.com/gitlab-org/gitlab/-/blob/master/app/graphql/types/merge_requests/detailed_merge_status_enum.rb but our Gitlab instance sends it, and we have many exceptions "Could not cast enum: `requested_changes` into a `Oneduo\LaravelGitlabWebhookClient\Enums\DetailedMergeStatus`".

--- src/Enums/DetailedMergeStatus.php
+++ src/Enums/DetailedMergeStatus.php
@@ -10,21 +10,28 @@ namespace Oneduo\LaravelGitlabWebhookClient\Enums;
  */
 enum DetailedMergeStatus: string
 {
+    case APPROVALS_SYNCING = 'approvals_syncing';
     case BLOCKED_STATUS = 'blocked_status';
     case BROKEN_STATUS = 'broken_status';
     case CHECKING = 'checking';
     case CI_MUST_PASS = 'ci_must_pass';
     case CI_STILL_RUNNING = 'ci_still_running';
+    case COMMITS_STATUS = 'commits_status';
     case CONFLICT = 'conflict';
     case DISCUSSIONS_NOT_RESOLVED = 'discussions_not_resolved';
     case DRAFT_STATUS = 'draft_status';
     case EXTERNAL_STATUS_CHECKS = 'external_status_checks';
     case JIRA_ASSOCIATION_MISSING = 'jira_association_missing';
+    case LOCKED_PATHS = 'locked_paths';
+    case LOCKED_LFS_FILES = 'locked_lfs_files';
     case MERGEABLE = 'mergeable';
     case MERGE_REQUEST_BLOCKED = 'merge_request_blocked';
+    case NEED_REBASE = 'need_rebase';
     case NOT_APPROVED = 'not_approved';
     case NOT_OPEN = 'not_open';
     case POLICIES_DENIED = 'policies_denied';
     case PREPARING = 'preparing';
+    case REQUESTED_CHANGES = 'requested_changes';
+    case SECURITY_POLICIES_EVALUATING = 'security_policy_evaluation';
     case UNCHECKED = 'unchecked';
 }
